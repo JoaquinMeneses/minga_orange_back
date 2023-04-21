779028f0f1bb0d2a7e0b58b57dfd364e389aba1d
Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>
2023-04-21 16:14:23 -0300

    data companies (#4)

---
+++ Models/Company.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+let schema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        logo: { type: String, required: true },
+        website: { type: String, required: true },
+        description: { type: String, required: false},
+        user_id: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+        active: { type: Boolean, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let collection = "companies"; //debe ser SIEMPRE en plural xq es un conjunto de recursos, documentos, etc y en lo posible en minuscula
+let Company = mongoose.model(schema, collection);
+
+export default Company;
