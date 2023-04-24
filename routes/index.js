68ef502ea26667d35333252332d853f99abfbaeb
Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>
2023-04-24 11:47:03 -0300

    endpoints (#7)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
+import userRouter from "./users.js"
+import authorRouter from "./authors.js"
+import companyRouter from "./companies.js"
+import mangaRouter from "./mangas.js"
+import chapterRouter from "./chapters.js"
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { 
+        title: "MINGA API" ,
+        subtitle: "Endpoints of MINGA API",
+    });
 });
+router.use("/auth", userRouter);
+router.use("/authors", authorRouter);
+router.use("/companies", companyRouter);
+router.use("/mangas", mangaRouter);
+router.use("/chapters", chapterRouter);
 
 export default router;
