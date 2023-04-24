6306a11007bd2461d8d346f7575e1d5997a0c717
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-24 12:37:10 -0300

    endpoints+controllers of categories and fix endpoints B-08

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,7 +1,7 @@
 const name = "mangas";
 const read = (req, res, next) =>
-   res.status(200).render("index", {
-      title: `/${name}`,
-      subtitle: `Endpoint de ${name}`,
-   });
+    res.status(200).render("index", {
+        title: `/${name}`,
+        subtitle: `Endpoint de ${name}`,
+    });
 export default read;
