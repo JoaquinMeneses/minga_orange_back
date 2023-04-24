7fc590d56c91168efb93565b18498b900945ee2e
Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>
2023-04-24 13:14:28 -0300

    M-02 (#10)

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import "dotenv/config.js";
 import "./config/database.js" 
 import createError from "http-errors";
 import express from "express";
-
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors"
 
 import indexRouter from "./routes/index.js";
 /* import usersRouter from "./routes/users.js"; */
@@ -17,6 +17,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
