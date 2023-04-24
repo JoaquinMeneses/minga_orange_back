e8efce7c475b09d82f9a8f8dc8dd4492389d3715
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-24 11:46:21 -0300

    endpoints

---
+++ controllers/companies/read.js
@@ -0,0 +1,7 @@
+const name = "companies";
+const read = (req, res, next) =>
+   res.status(200).render("index", {
+      title: `/${name}`,
+      subtitle: `Endpoint de ${name}`,
+   });
+export default read;
