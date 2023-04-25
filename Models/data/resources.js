fd5ced653c138e80dd6616e534c170284ba6af24
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-25 15:33:39 -0300

    correcion de calidad de imagenes de resources

--- Models/data/resources.js
+++ Models/data/resources.js
@@ -3,11 +3,11 @@ import '../../config/database.js'
 import Resource from "../Resource.js"
 
 let resources = [{
-    cover_photo: "https://i.postimg.cc/PLD8Dwmm/Manga1.png"
+    cover_photo: "https://res.cloudinary.com/diutf2pd0/image/upload/v1682447032/Manga1_wckxbz.png"
 }, {
-    cover_photo: "https://i.postimg.cc/v4kVDz6Y/Manga2.png"
+    cover_photo: "https://res.cloudinary.com/diutf2pd0/image/upload/v1682447032/Manga2_lelzyc.png"
 },{
-    cover_photo: "https://i.postimg.cc/Kky3D8NC/Manga3.png"
+    cover_photo: "https://res.cloudinary.com/diutf2pd0/image/upload/v1682447032/Manga3_cocopz.png"
 }]
 
 let newResources = async() => await Resource.insertMany(resources)
